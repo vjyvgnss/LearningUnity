a71555cd8c6322a0907c9e5f79a4d3ea455ff59f
vjyvgnss <58178309+vjyvgnss@users.noreply.github.com>
2020-04-17 01:18:25 +0100

    Initial Commit
    Initial Commit with few files

---
+++ Assets/PlayerMovemen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerMovemen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
